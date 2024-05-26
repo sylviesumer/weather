0f65aa0a41638d017e220386c3b7eda6228d49f0
Sylvie Sumer <sylviesumer@gmail.com>
2024-05-26 13:39:01 +1000

    add background-image

--- App.js
+++ App.js
@@ -1,23 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  KeyboardAvoidingView,
+  ImageBackground,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import SearchInput from "./components/SearchInput";
+import getImageForWeather from "./utils/getImageForWeather";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app3</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="height">
+        <ImageBackground source={getImageForWeather('Clear')} style={styles.imageContainer} imageStyle={styles.image}>
+            <View style={styles.detailsContainer}>
+                <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
+                <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
+                <SearchInput placeholder="Search any city2" />
+            </View>
+        </ImageBackground>
+      </KeyboardAvoidingView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: "#34495e",
+
+  },
+  imageContainer: {
+    flex: 1,
+
+  },
+  detailsContainer: {
+    flex: 1,
     justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20
+  },   
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    color: '#fff',
+  },
+  smallText: {
+    fontSize: 18,
   },
   text: {
-    fontWeight: 'bold',
-  }
+    fontWeight: "bold",
+    color: "red",
+  },
 });
